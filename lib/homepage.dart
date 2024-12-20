06b0e674900b77c79ebf5078fa9923b52a2dc438
bedaoui khalil <k.bedaoui@bekerlaboratoires.com>
2024-12-20 21:53:10 +0100

    add bottomNavigationBar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,6 +8,7 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  int selectedIndex = 0;
   List categories = [
     {
       "iconname": Icons.laptop,
@@ -63,151 +64,185 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      padding: const EdgeInsets.all(20),
-      child: ListView(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search',
-                    prefixIcon: const Icon(Icons.search),
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                  ),
-                ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (val) {
+            setState(() {
+              selectedIndex = val;
+            });
+          },
+          iconSize: 30,
+          currentIndex: selectedIndex,
+          selectedItemColor: Colors.orange,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Colors.white,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
               ),
-              const Padding(
-                padding: EdgeInsets.only(left: 5),
-                child: Icon(
-                  Icons.menu,
-                  size: 40,
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.shopping_bag_outlined,
+              ),
+              label: "Shop",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_outline,
+              ),
+              label: "Profile",
+            ),
+          ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: ListView(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        prefixIcon: const Icon(Icons.search),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 5),
+                    child: Icon(
+                      Icons.menu,
+                      size: 40,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                "Categories",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                height: 100,
+                child: ListView.builder(
+                    itemCount: categories.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, i) {
+                      return Container(
+                        child: Column(
+                          children: [
+                            Container(
+                                margin: const EdgeInsets.only(right: 10),
+                                padding: const EdgeInsets.all(15),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(40)),
+                                ),
+                                child: Icon(
+                                  categories[i]['iconname'],
+                                  size: 40,
+                                )),
+                            Text(
+                              categories[i]['title'],
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey[800]),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+              Container(
+                height: 30,
+              ),
+              const Text(
+                "Best Sellings",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                child: GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisExtent: 250,
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 10.0,
+                  ),
+                  itemCount: items.length,
+                  itemBuilder: (context, i) {
+                    return Card(
+                      color: Colors.white,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 200,
+                              color: Colors.grey[200],
+                              padding: const EdgeInsets.all(30),
+                              child: Image.asset(
+                                items[i]["image"],
+                                fit: BoxFit.fill,
+                                height: 100,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 3.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    items[i]["title"],
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 1),
+                                  Text(
+                                    items[i]["subTitle"],
+                                    style: const TextStyle(
+                                      fontSize: 13,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Text(
+                                    items[i]["price"],
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ]),
+                    );
+                  },
                 ),
               ),
             ],
           ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            "Categories",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Container(
-            height: 100,
-            child: ListView.builder(
-                itemCount: categories.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, i) {
-                  return Container(
-                    child: Column(
-                      children: [
-                        Container(
-                            margin: const EdgeInsets.only(right: 10),
-                            padding: const EdgeInsets.all(15),
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(40)),
-                            ),
-                            child: Icon(
-                              categories[i]['iconname'],
-                              size: 40,
-                            )),
-                        Text(
-                          categories[i]['title'],
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey[800]),
-                        )
-                      ],
-                    ),
-                  );
-                }),
-          ),
-          Container(
-            height: 30,
-          ),
-          const Text(
-            "Best Sellings",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Container(
-            height: 300,
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisExtent: 250,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 10.0,
-              ),
-              itemCount: items.length,
-              itemBuilder: (context, i) {
-                return Card(
-                  color: Colors.white,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: 200,
-                          color: Colors.grey[200],
-                          padding: const EdgeInsets.all(30),
-                          child: Image.asset(
-                            items[i]["image"],
-                            fit: BoxFit.fill,
-                            height: 100,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 3.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                items[i]["title"],
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 1),
-                              Text(
-                                items[i]["subTitle"],
-                                style: const TextStyle(
-                                  fontSize: 13,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              const SizedBox(height: 4),
-                              Text(
-                                items[i]["price"],
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.orange,
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ]),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    ));
+        ));
   }
 }
